7397cbf2f4529630f5e1e398c73993820a513049
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-06-27 19:26:47 -0700

    most recent addings

--- controllers/places.js
+++ controllers/places.js
@@ -1,36 +1,33 @@
 const router = require('express').Router();
+const places = require('../models/places.js');
 
+//GET /places
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Las Vegas',
-    state: 'NV',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/table-in-restaurant.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
-  }];
-  res.render('places/index', { places });
-});
+    res.render('places/index', { places })
+})
 
+//POST /places
 router.post('/', (req, res) => {
-  if (!req.body.pic) { req.body.pic = 'http://placekitten.com/400/400' }
-  if (!req.body.city) { req.body.city = 'Anytown' }
-  if (!req.body.state) { req.body.state = 'USA' }
-  places.push(req.body);
-  res.redirect('/places');
-});
+    // console.log(req.body)
 
+    if (!req.body.pic) {
+        req.body.pic = 'https://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+
+    places.push(req.body)
+    res.redirect('/places')
+})
+
+//GET /places/new
 router.get('/new', (req, res) => {
-  res.render('places/new');
-});
+    res.render('places/new')
+})
 
-router.get('/:id', (req, res) => {
-  res.render('places/${req.params.id}');
-});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Require needed modules
-require('dotenv').config();
 const express = require('express');
+
 
 // Initialize the app object
 const app = express();
+require('dotenv').config();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
@@ -18,12 +19,15 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-// Create a wildcard route
+// Create a route
 app.get('*', (req, res) => {
   res.status(404).render('error404');
 });
 
-// Listen for connections
+//Listen for connections
 app.listen(process.env.PORT, () => {
-  console.log('Awake and listening');
-});
+  console.log(`http://localhost:${process.env.PORT}`)
+  console.log(`http://localhost:${process.env.PORT}/places`)
+  console.log(`http://localhost:${process.env.PORT}/404test`)
+  console.log(`http://localhost:${process.env.PORT}/places/new`)
+})
