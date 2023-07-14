53d1bd70a8f2c1a1447e666d443ec7761a8f4500
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-07-14 12:18:40 -0700

    part 11 changed and completed

--- Views/Places/edit.jsx
+++ Views/Places/edit.jsx
@@ -27,11 +27,20 @@ function edit_form (data) {
               <div className="form-group col">
                 <label htmlFor="cuisines">Cuisines</label>
                 <input className="form-control" id="cuisines" name="cuisines" value ={data.place.cuisines} required />
+
               </div>
+              <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded</label>
+            <input className="form-control"
+              id="founded"
+              name="founded"
+              value={data.place.founded}
+            />
+        </div>
 
             </div>
 
-          <input className="btn btn-primary" type="submit" value="Edit Place" />
+          <input className="btn btn-primary" type="submit" value="Update Place" />
         </form>
           </main>
         </Def>
--- Views/Places/new.jsx
+++ Views/Places/new.jsx
@@ -1,20 +1,29 @@
-const React = require('react');
-const Def = require('../default');
+const React = require('react')
+const Def = require('../default')
 
 function new_form(data) {
-  let message = '';
-  if(data.message) {
-    message = (
-      <h4 className="alert-danger">
-        { data.message }
-      </h4>
+  let message = ''
+    if (data.message) {
+      message = (
+        <h4 className="alert-danger">
+          {data.message}
+        </h4>
+      )
+    }
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+      {Math.round(averageRating)} stars
+      </h3>
     )
-  }
+
   return (
     <Def>
       <main>
         <h1>Add a New Place</h1>
-        { message }
         <form method="POST" action="/places">
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
@@ -22,29 +31,37 @@ function new_form(data) {
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" type="url" id="pic" name="pic" />
+            <input className="form-control" id="pic" name="pic" />
           </div>
-          <div className="form-group">
-            <label htmlFor="city">Place City</label>
+          <div className="form-group col-sm-4">
+            <label htmlFor="city">City</label>
             <input className="form-control" id="city" name="city" />
           </div>
-          <div className="form-group">
-            <label htmlFor="state">Place State</label>
+          <div className="form-group col-sm-4">
+            <label htmlFor="state">State</label>
             <input className="form-control" id="state" name="state" />
           </div>
+          <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded Year</label>
+            <input
+              type="number"
+              className="form-control"
+              id="founded"
+              name="founded"
+              value={new Date().getFullYear()} />
+          </div>
           <div className="form-group">
-            <label htmlFor="cuisines">Place Cuisines</label>
+            <label htmlFor="cuisines">Cuisines</label>
             <input className="form-control" id="cuisines" name="cuisines" required />
           </div>
-          <div className="form-group">
-            <label htmlFor="founded">Founded Year</label>
-            <input type="number" className="form-control" id="founded" name="founded" value={ new Date().getFullYear() } />
-          </div>
+
+
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
+
       </main>
     </Def>
-  );
+  )
 }
 
-module.exports = new_form;
+module.exports = new_form
